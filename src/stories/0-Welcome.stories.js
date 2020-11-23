0bb29266add697e055324a05faff1260b17ddddb
779g <adrian.villa@779.mx>
2020-11-23 14:11:25 +0000

    Update introduction

--- src/stories/0-Welcome.stories.js
+++ src/stories/0-Welcome.stories.js
@@ -3,7 +3,7 @@ import { linkTo } from '@storybook/addon-links'
 import { Welcome } from '@storybook/react/demo'
 
 export default {
-  title    :'Fwrlines Design System / Core',
+  title    :'ds.core',
   //component:Welcome,
 }
 
